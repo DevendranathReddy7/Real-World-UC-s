21f0b86506859b075c219e1547ecd9ea2b7dd433
Devendra <devendranathreddy54@gmail.com>
2024-04-05 07:09:19 +0530

    currency convetor

--- src/usecases/CurrencyConverter/CurrencyConverter.js
+++ src/usecases/CurrencyConverter/CurrencyConverter.js
@@ -5,10 +5,13 @@ import DropDown from "./DropDown";
 const CurrencyConverter = (props) => {
   const [currencies, setCurrencies] = useState();
   const [selectedCurrencies, setSelectedCurrencies] = useState({
-    from: "",
-    to: "",
+    from: "AUD",
+    to: "INR",
+    convertedValue: "",
   });
   const [amount, setAmount] = useState(0);
+  const [amountError, setAmountError] = useState(null);
+  const [currencyError, setCurrencyError] = useState(null);
 
   useEffect(() => {
     const getCurrencies = async () => {
@@ -22,54 +25,95 @@ const CurrencyConverter = (props) => {
   }, []);
 
   const convertHandler = async () => {
+    if (selectedCurrencies.from === selectedCurrencies.to)
+      return setCurrencyError(true);
+    if (amount === 0) return setAmountError(true);
     const res = await fetch(
       `https://api.frankfurter.app/latest?amount=${amount}&from=${selectedCurrencies.from}&to=${selectedCurrencies.to}`,
       { method: "GET" }
     );
     const data = await res.json();
+    console.log(data.rates);
+    setSelectedCurrencies((prev) => ({ ...prev, convertedValue: data.rates }));
   };
 
   const amountHandler = (e) => {
-    setAmount(parseInt(e.target.value));
+    const amountValue = e.target.value;
+    if (isNaN(amountValue)) return setAmountError(true);
+    setAmount(parseInt(amountValue));
+    setAmountError(false);
   };
 
-  const swapHandler = () => {};
+  const swapHandler = () => {
+    setSelectedCurrencies((prev) => ({
+      ...prev,
+      from: selectedCurrencies.to,
+      to: selectedCurrencies.from,
+      convertedValue: "",
+    }));
+  };
 
-  const currencyHandler = () => {};
+  const currencyHandler = (curency, type) => {
+    setCurrencyError(false);
+    switch (type) {
+      case "from":
+        setSelectedCurrencies((prev) => ({ ...prev, from: curency }));
+        break;
+      case "to":
+        setSelectedCurrencies((prev) => ({ ...prev, to: curency }));
+        break;
+    }
+  };
+
   return (
     <div className="wrapper">
       <h3 className="heading">Currency Converter</h3>
       <div className="dropdown__wrapper">
-        <div>
+        <div className="drpdwnDiv">
+          <label className="label">From</label>
           <DropDown
             curList={currencies}
             curType="from"
             selectedCur={currencyHandler}
+            defaultCurrency={selectedCurrencies.from}
           />
         </div>
         <div onClick={swapHandler}>
           <p className="swap">🔄️</p>
         </div>
-        <div>
+        <div className="drpdwnDiv">
+          <label className="label">To</label>
           <DropDown
             curList={currencies}
             curType="to"
             selectedCur={currencyHandler}
+            defaultCurrency={selectedCurrencies.to}
           />
         </div>
       </div>
-
+      {currencyError && (
+        <p className="errorPara">From & To currency can't be same</p>
+      )}
       <div>
-        <input
-          className="input"
-          placeholder="Enter Amount"
-          onChange={amountHandler}
-        />
+        <label className="label">Amount</label>
+        <input className="input" onChange={amountHandler} />
+        {amountError && <p className="errorPara">Please enter valid amount</p>}
       </div>
-      <div>
-        <button className="convert" onClick={convertHandler}>
+      <div className="convertDiv">
+        <button
+          className={amountError || currencyError ? "disabled" : "convert"}
+          onClick={convertHandler}
+          disabled={amountError || currencyError}
+        >
           Convert
         </button>
+        {selectedCurrencies.convertedValue !== "" && (
+          <p className="curencyPara">
+            Converted currency:{" "}
+            {selectedCurrencies?.convertedValue[selectedCurrencies.to]}{" "}
+            {selectedCurrencies.to}
+          </p>
+        )}
       </div>
     </div>
   );
--- src/usecases/CurrencyConverter/DropDown.js
+++ src/usecases/CurrencyConverter/DropDown.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./CurrencyConverter.css";
-const DropDown = ({ curList, curType }) => {
+const DropDown = ({ curList, curType, selectedCur, defaultCurrency }) => {
+  const dropdownHandler = (e) => {
+    selectedCur(e.target.value, curType);
+  };
+
   return (
-    <select className="dropdown">
+    // <div className="dropdown__container">
+    <select
+      className="dropdown"
+      onChange={dropdownHandler}
+      value={defaultCurrency}
+    >
       {curList?.map((cur) => {
-        return <option key={cur}>{cur}</option>;
+        return (
+          <option key={cur} className="option">
+            {cur}
+          </option>
+        );
       })}
     </select>
+    //</div>
   );
 };
 export default DropDown;
